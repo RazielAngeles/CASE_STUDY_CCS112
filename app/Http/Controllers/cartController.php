1c0b28746d589c7e0a48be5a9383836c7771c59e
zelsoooo <162017559+RazielAngeles@users.noreply.github.com>
2024-05-07 09:48:59 +0800

    Nice

--- app/Http/Controllers/cartController.php
+++ app/Http/Controllers/cartController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ItemCart;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class cartController extends Controller
 {
     public function getCart()
-    {$products = Item::all(); 
+    {$products = ItemCart::all(); 
         
         return response()->json($products);
     }
